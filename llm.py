2dde22e2e385cd5978b69b24a4b1e17e7c4ba4e7
latheeshmangeri <latheeshmangeri369@gmail.com>
2024-05-29 15:47:12 +0530

    a

--- llm.py
+++ llm.py
@@ -1,9 +1,18 @@
 from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+load_dotenv()
 
 llm = ChatOpenAI(
-  api_key='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'
-  )
+  model="gpt-3.5-turbo",
+  temperature=0.7,
+  max_tokens=1000,
+  verbose=True
+)
 
-response = llm.invoke('Hello, How you doing?')
+response = llm.stream('write a poem about AI')
 
-print(response)
+for chunk in response:
+  print(chunk.content,end="",flush=True)
+
+#response = llm.stream('write a poem about AI')
+#print(response)
---
+++ prompt-templates.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+
+#Instantiate Model
+llm = ChatOpenAI(
+  temperature=0.7,
+  model='gpt-3.5-turbo-1106'
+)
+
+#Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+  [
+    ("system","You are a Brilliant chef. Create a unique recipe based on the following ingredient."),
+    ("human","{input}")
+  ]
+)
+
+prompt = ChatPromptTemplate.from_messages(
+  [
+    ("system","Generate 10 Synonms for following word. Return response in comma separated values."),
+    ("human","{input}")
+  ]
+)
+
+# Create a LLM Chain
+chain = prompt | llm
+
+response = chain.invoke({"input": "Happy"})
+
+print(response.content)
